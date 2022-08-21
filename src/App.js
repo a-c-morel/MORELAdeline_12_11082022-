f9caa61b0c277365182b1c6051f5cacd65e713bd
Adeline Claire MOREL <adelinemorel72@gmail.com>
2022-08-21 14:34:26 +0200

    feat: first attempt to fetch mocked data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import React,{useState,useEffect} from 'react';
+
 function App() {
+  const [data,setData]=useState([]);
+  const getData=()=>{
+    fetch('mockAPI.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        setData(myJson)
+      });
+  }
+  useEffect(()=>{
+    getData()
+  },[])
   return (
-    <div className="App">
-      test
-    </div>
+    <div className="App" data={data}>display some text</div>
   );
 }
 
